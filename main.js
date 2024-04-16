9d997c9fa634917b63c4e1ac2cd2614fbce6c0b7
Nicolas Johnson <nicolasssousa2011@gmail.com>
2024-04-16 17:05:28 -0300

    Mobille navigation and JavaScript

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-import './style.css'
+import '/public/sass/styles.scss';
+
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+const navLink = document.querySelectorAll('.nav__link');
+
+if(navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+
+if(navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
+
+const linkAction = () => {
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+
